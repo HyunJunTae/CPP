a7d3a09a02ae8f95323ef5245d7889d0ede79c17
HyunJunTae <juny000041@gmail.com>
2022-10-01 18:33:56 +0900

    221001

--- CPP_Lab/Week5_Lab_20221627_태현준/main.cpp
+++ CPP_Lab/Week5_Lab_20221627_태현준/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include "my-string.hpp"
 #include "my-string.cpp"
 
 using std::cout;
@@ -8,15 +7,36 @@ using std::endl;
 // I will fix this function to test your class implementation
 void test() {
     MyString s1("ABCDEF");
-    MyString s2("DEF");
-    // s1.Append(&s2);
+    MyString s2("abc");
+
     s1.Print();
     cout << s1.GetLength() << endl;
     s2.Print();
     cout << s2.GetLength() << endl;
-    // s1.Append(&s2);
-    // s1.Insert(&s2, 1);
-    s1.Remove(1,2);
+
+    s1.Append(&s2);
+
+    s1.Print();
+    cout << s1.GetLength() << endl;
+    s2.Print();
+    cout << s2.GetLength() << endl;
+
+    s1.Insert(&s2, 1);
+    
+    s1.Print();
+    cout << s1.GetLength() << endl;
+    s2.Print();
+    cout << s2.GetLength() << endl;
+
+    s1.Remove(1,3);
+
+    s1.Print();
+    cout << s1.GetLength() << endl;
+    s2.Print();
+    cout << s2.GetLength() << endl;
+
+    s1.Clear();
+
     s1.Print();
     cout << s1.GetLength() << endl;
     s2.Print();
--- CPP_Lab/Week5_Lab_20221627_태현준/my-string.cpp
+++ CPP_Lab/Week5_Lab_20221627_태현준/my-string.cpp
@@ -5,88 +5,109 @@
 
 // Destructor.
 MyString::~MyString() {
-
+	free(this->arr);
 }
 
 // Get the length of the string.
 size_t MyString::GetLength() {
-	// realloc(this->arr, this->length+1);
+	char a[this->length+1];
+	
+	for(int i=0; i<this->length; i++) {
+		a[i] = this->arr[i];
+	}
+	a[this->length] = '\0';
 	
 	// return strlen(this->arr); // ??????????????????????????????????????????????????????????????????????????????? 왜 안됨????????
-	return this->length;
+	// 안되는 이유 : hpp에서 입력받을 때는, ABC\0 이렇게 입력받은채로 바로 strlen하니까 3으로 잘 나오는데,
+	// malloc해서 저장받은 다음에 할 때는, malloc 길이가 널 문자 포함 안되어있으니까 오류남.
+	return strlen(a);
 }
 
 // Make the string into an empty string.
 void MyString::Clear() {
-	// realloc(this->arr, this->length+1);
-	
-	int k = strlen(this->arr);
-        for(int i=0; i<k; i++) {
-            this->arr[i] = '\0';
-        }
+
+	char a[1];
+	strcpy(this->arr, a);
+
+	// int k = GetLength();
+    //     for(int i=0; i<k; i++) {
+    //         this->arr[i] = '\0';
+    //     }
+
+	this->length = 0;
+
+	return;
 }
 
 // Append 's' at the end of this string.
 void MyString::Append(MyString *s) {
-	// realloc(this->arr, this->length+1);
-	// realloc(s->arr, s->length+1);
 
-	int i = this->length + s->length;
-	realloc(this->arr, i);
-	strcat(this->arr, s->arr);
-	this->arr[i] = '\0';
+	int i = this->length;
+	int j = s->length;
 
-	this->length = strlen(this->arr);
+	char a[i+j+1];
+
+	for(int k=0; k<i; k++)
+		a[k] = this->arr[k];
+	for(int k=0; k<j; k++)
+		a[i+k] = s->arr[k];
+	a[i+j] = '\0';
+	
+	realloc(this->arr, i+j);
+	strncpy(this->arr, a, i+j);
+
+	this->length += j;
+
+	return;
 }
 
 // Insert 's' at position 'idx' of this string. idx=0 means the start.
 void MyString::Insert(MyString *s, int idx) {
-	// realloc(this->arr, this->length+1);
-	// realloc(s->arr, s->length+1);
 
-	int i;
-	i = this->length;
+	int i = this->length;
+	int j = s->length;   // ABCDEF
 
-	if (idx > i)
+	if (idx > i+1)
 		return;
 
-	realloc(this->arr, this->length + s->length);
+	char a[i+j+1];
 
-	for(int j=i; j>idx; j--) 
-		this->arr[j+s->length-1] = this->arr[j-1];
+	for (int k=0; k<idx; k++)  // ABCDEF abc       1
+		a[k] = this->arr[k];
+	for (int k=0; k<j; k++)
+		a[idx+k] = s->arr[k];
+	for (int k=idx; k<i; k++)
+		a[k+j] = this->arr[k];
+	a[i+j] = '\0';
 
-	int k = s->length;
-	for(int j=i; j>idx; j--) {
-		this->arr[j-1] = s->arr[k-1];
-		k--;
-	}
-	this->length = strlen(this->arr);
+	realloc(this->arr, i+j);
+	strcpy(this->arr, a);
+	
+	this->length += j;
+
+	return;
 }
 
 // Starting from the position 'idx', remove 'n' characters.
 void MyString::Remove(int idx, int n) {
-	// realloc(this->arr, this->length+1);	
 
-	int i;
-	i = strlen(this->arr);
+	int i = this->length;
 
-	if (idx > i)
+	if (n > i-idx)          // abcdefgh	ABCDEF
 		return;
-	printf("1 : %s\n", this->arr);
-	for(int j=idx; j<idx+n; j++) 
-		this->arr[j] = '\0';
-	printf("1 : %s\n", this->arr);
-	for(int j=0; j<i-idx-n; j++) {
-		this->arr[idx+j] = arr[idx+n+j];
-	}
-	printf("1 : %s\n", this->arr);	
-	if (i-idx-n >= n) {
-		for (int j=0; j<n; j++) {
-			this->arr[i-j-1] = '\0';
-		}
-	}
-	printf("1 : %s\n", this->arr);
 
-	this->length = strlen(this->arr);
+	char a[i-n+1];
 
+	for (int j=0; j<idx; j++)
+		a[j] = this->arr[j];
+	for (int j=0; j<i-n-1; j++) 
+		a[idx+j] = this->arr[idx+n+j];
+	a[i-n] = '\0';
+
+	realloc(this->arr, i-n);
+	strcpy(this->arr, a);
+
+	this->length -= n;
+
+	return;
 }
