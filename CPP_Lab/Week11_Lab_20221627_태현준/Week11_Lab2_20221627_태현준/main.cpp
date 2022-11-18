e47e0f74fea33b1441e7c53d5ffb845b154e9069
HyunJunTae <juny000041@gmail.com>
2022-11-18 18:04:06 +0900

    221118

--- CPP_Lab/Week11_Lab_20221627_태현준/Week11_Lab2_20221627_태현준/main.cpp
+++ CPP_Lab/Week11_Lab_20221627_태현준/Week11_Lab2_20221627_태현준/main.cpp
@@ -9,8 +9,11 @@ using std::endl;
 int main() {
     double arr1[3] = { 1.2, 3.3, 5.4 };
     double arr2[3] = { 2.7, 4.8, 7.5 };
+    double arr3[3] = {};
+
     DynamicArray<double> *da1 = new DynamicArray<double>(arr1, 3);
     auto da2 = new DynamicArray<double>(arr2, 3); // Type deduction!
+    auto da7 = new DynamicArray<double>(arr3, 0);
 
     da1->Append(da2);
     da1->Print(); // Should print "1.2, 3.3, 5.4, 2.7, 4.8, 7.5"
@@ -24,12 +27,20 @@ int main() {
     auto da5 = da1->Subseq(-1, 3);
     da5->Print();
 
+    auto da6 = da7->Subseq(1, 3);
+    da6->Print();
+
+    da1->Append(da7);
+    da1->Print();
+
 
     delete da1;
     delete da2;
     delete da3;
     delete da4;
     delete da5;
+    delete da6;
+    delete da7;
 
     return 0;
 }
