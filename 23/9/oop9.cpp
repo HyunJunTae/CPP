d3a2b926c45e82bd0e45143e0974f76411589edf
HyunJunTae <juny000041@gmail.com>
2023-09-28 18:26:54 +0900

    230928

--- 23/9/oop9.cpp
+++ 23/9/oop9.cpp
@@ -4,9 +4,10 @@ using namespace std;
 
 class UnivMem
 {
-private:
+protected:
     int id;
     string name;
+
 public:
     UnivMem(int _id, string _name)
     {
@@ -19,19 +20,27 @@ public:
         cout << "ID : " << id << endl;
         cout << "Name : " << name << endl;
     }
+
 };
 
 class Student : public UnivMem
 {
 private:
     double GPA;
+
 public:
     Student(int _id, string _name, double _GPA) : UnivMem(_id, _name)
     {
         GPA = _GPA;
     }
-    double getGPA() {
+    double getGPA()
+    {
         return GPA;
+    }
+    void Print() {
+        cout << "ID : " << id << endl;
+        cout << "Name : " << name << endl;
+        cout << "GPA : " << GPA << endl;
     }
 };
 
@@ -39,10 +48,17 @@ class Professor : public UnivMem
 {
 private:
     string office;
+
 public:
     Professor(int _id, string _name, string _office) : UnivMem(_id, _name)
     {
         office = _office;
+    }
+    void Print()
+    {
+        cout << "ID : " << id << endl;
+        cout << "Name : " << name << endl;
+        cout << "Office : " << office << endl;
     }
 };
 
@@ -62,22 +78,17 @@ int main(void)
 
     UnivMem *members[2];
     // 포인터를 사용하지 않고는 자식클래스의 객체를 부모클래스 배열에 넣는 것 불가능. 반드시 포인터를 사용해야함.
-    
 
     members[0] = &x;
     members[1] = &y;
 
     printMem(members, 2);
-
+    cout << "--------------------------" << endl;
     members[0]->Print();
-
-    // Univmem으로는 getGPA 못부름.
-    cout << x.getGPA() << endl;
-
-    Student *ps;
-    ps = &x;
-
-    cout << ps->getGPA() << endl;
+    members[1]->Print();
+    cout << "--------------------------" << endl;
+    x.Print();
+    y.Print();
 
     return 0;
 }
